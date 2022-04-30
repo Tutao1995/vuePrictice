8f198ac7d533a0fe7cf3f82466d4b529666f26ea
涂滔 <851542486@qq.com>
2022-04-30 15:08:30 +0800

    zujian

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,45 +4,7 @@
             <header>头</header>
         </div>
         <div class="home-content">
-            <div class="home-content-nav">
-                <el-menu
-                    default-active="2"
-                    class="el-menu-vertical-demo"
-                    @open="handleOpen"
-                    @close="handleClose"
-                >
-                    <el-sub-menu index="1">
-                        <template #title>
-                            <el-icon><location /></el-icon>
-                            <span>Navigator One</span>
-                        </template>
-                        <el-menu-item-group>
-                            <template #title><span>Group One</span></template>
-                            <el-menu-item index="1-1">item one</el-menu-item>
-                            <el-menu-item index="1-2">item two</el-menu-item>
-                        </el-menu-item-group>
-                        <el-menu-item-group title="Group Two">
-                            <el-menu-item index="1-3">item three</el-menu-item>
-                        </el-menu-item-group>
-                        <el-sub-menu index="1-4">
-                            <template #title><span>item four</span></template>
-                            <el-menu-item index="1-4-1">item one</el-menu-item>
-                        </el-sub-menu>
-                    </el-sub-menu>
-                    <el-menu-item index="2">
-                        <el-icon><icon-menu /></el-icon>
-                        <template #title>Navigator Two</template>
-                    </el-menu-item>
-                    <el-menu-item index="3" disabled>
-                        <el-icon><document /></el-icon>
-                        <template #title>Navigator Three</template>
-                    </el-menu-item>
-                    <el-menu-item index="4">
-                        <el-icon><setting /></el-icon>
-                        <template #title>Navigator Four</template>
-                    </el-menu-item>
-                </el-menu>
-            </div>
+            <nav class="home-content-nav" />
             <div class="home-content-box">
                 <div class="grid-content bg-purple">1</div>
             </div>
@@ -52,6 +14,8 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+
+import nav from './components/nav.vue'
 
 const navList = [
     {
